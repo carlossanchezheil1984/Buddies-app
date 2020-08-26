9bcda4160f3691ec79f6015ffa70b9e2c49f92a3
Rubster18 <thegrandruben@gmail.com>
2020-08-26 16:12:27 +0200

    added components for students and buddies and post submit page, buttons need links

---
+++ src/ThanksYou.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { render } from '@testing-library/react';
+
+
+goHome = () => {
+    
+}
+
+const submit = {
+    render() {
+        return(
+            <div className="final-site">
+                <h1>Thank you for your registration!</h1>
+                <span className="--base-text"> we will contact you soon, please be patient.</span>
+                <button className="big-button" onClick={}> Go back </button>
+            </div>
+        )}
+}
+
