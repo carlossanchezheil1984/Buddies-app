04f252d2e58d5b83e12f598a3be8b8e1763c54ff
Migracode <sanchezgalejandroj@gmail.com>
2020-08-26 16:04:13 +0200

    New branch and header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./App.css";
 import RegisterForm from "./RegisterForm.js"
+import Header from "./Header.js"
 
 //Main Site
 function App(){
   return(
     <div className="App">
+      <Header/>
       <RegisterForm />
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header>
+            <div className="header-flex-container">
+                <div className="logo">bb</div>
+
+                <div className="small-buttons-container">
+                <div>SmallButtonComponent</div>
+                <div>SmallButtonComponent</div>
+                </div>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
