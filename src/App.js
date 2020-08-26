81c3173225bad01293dff7a45b11de5d6c4d86b1
Anandamaya <arnoananda@gmail.com>
2020-08-26 15:40:08 +0200

    added the components of the buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./App.css";
 import RegisterForm from "./RegisterForm.js"
+import Button from "./Button";
 
 //Main Site
 function App(){
   return(
     <div className="App">
+      <Button buttonType="small" text="Holiwis" submitBtn="true" />
+      <Button buttonType="big" text="Wuola" />
       <RegisterForm />
     </div>
   );
