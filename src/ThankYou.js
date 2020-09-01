5f9a9e23bd4d3645c0599c46f4ea4914b0a95563
Anandamaya <arnoananda@gmail.com>
2020-09-02 01:31:50 +0200

    fixing css bugs

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 const Submit = () => {
     return(
-        <div className="final-site">
-            <div className="Rectangle-15">
-                <h1>succes! </h1>
-                <h2> Uw gegevens zijn correct verzonden </h2>
-            </div>
-            <div className="Rectangle-16">
-                <span className="--base-text"> we will contact you soon, please be patient.</span>
-                <p> we nemen contact met u op om u de volgende stappen te laten weten, bedankt voor uw vertrouwen </p>
-                   <a className="big-button"> ga terug naar de startpagina </a>
+        <div>
+            <Header />
+            <div className="thank-you-container">
+                <div className="success-container">
+                    <h1>Success! </h1>
+                    <h2> Uw gegevens zijn correct verzonden </h2>
+                </div>
+                <div className="contact-you-container">
+                    <p> We will contact you soon, please be patient.we nemen contact met u op om u de volgende stappen te laten weten, bedankt voor uw vertrouwen </p>
+                    <a href="." className="big-button"> ga terug naar de startpagina </a>
+                </div>
             </div>
         </div>
     )}
