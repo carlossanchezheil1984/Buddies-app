67b0090d8a35b0337d0965771dd614a3afb29698
Anandamaya <arnoananda@gmail.com>
2020-09-13 22:25:16 +0200

    creating the 3 buttons to filter by buddies, patients or all

--- src/AdminTable.js
+++ src/AdminTable.js
@@ -1,43 +1,101 @@
 import React from 'react';
 import Header from './Header';
 
-const listOfPeople = [
-    { id: 1, name: "Ale", age: 21, email: "test@email.com", hometown: "barcelona", hobbies: "dancing, singing, playing, watching movies", buddy_patient: "buddy" },
-    { id: 2, name: "Andy", age: 22, email: "test@email.com", hometown: "barcelona", hobbies: "dancing, singing, playing, watching movies", buddy_patient: "patient" },
-    { id: 3, name: "Bruce", age: 25, email: "test@email.com", hometown: "barcelona", hobbies: "dancing, singing, playing, watching movies", buddy_patient: "buddy" },
-    { id: 4, name: "Ki", age: 27, email: "test@email.com", hometown: "barcelona", hobbies: "dancing, singing, playing, watching movies", buddy_patient: "patient" },
-    { id: 5, name: "Klim", age: 29, email: "test@email.com", hometown: "barcelona", hobbies: "dancing, singing, playing, watching movies", buddy_patient: "patient" },
-    { id: 6, name: "Ruben", age: 20, email: "test@email.com", hometown: "barcelona", hobbies: "dancing, singing, playing, watching movies", buddy_patient: "buddy" }
-  ];
+const AdminTableFetcher = props => {
+    const [tableData, setTableData] = React.useState(null);
+    const [mainArray, setMainArray] = React.useState(null);
 
-const AdminTable = () => {
+    const seeBuddies = () => {
+        const filteredData = mainArray.filter( element => element.im_a_buddy === 1);
+        setTableData(filteredData);
+    }
+
+    const seePatients = () => {
+        const filteredData = mainArray.filter( element => element.im_a_buddy !== 1);
+        setTableData(filteredData);
+    }
+
+    const seeAll = () => {
+        setTableData(mainArray);
+    }
+
+    React.useEffect(() => {
+        fetch("http://localhost:9000/get-table")
+            .then(response => response.json())
+            .then(data => {setTableData(data); setMainArray(data)})
+            .catch(rejected => console.log(rejected))
+    }, []);
+
+    return (
+
+        !tableData ? 
+            (<p>Loading...</p>) 
+        : (
+            <div className="">
+                <Header />
+
+                <div className="container-form">
+                <div className="table-button-container">
+                    <button className="small-button" onClick={seeBuddies}>See Buddies</button>
+                    <button className="small-button" onClick={seePatients}>See Patients</button>
+                    <button className="small-button" onClick={seeAll}>See All</button>
+                </div>
+                    <AdminTable data={tableData}/>
+
+                </div>
+            </div> 
+        )
+    )
+}
+
+const AdminTable = props => {
+
+
+    function calculate_age(dateofbirth) { 
+        const date = new Date(dateofbirth);
+        var diff_ms = Date.now() - date.getTime();
+        var age_dt = new Date(diff_ms); 
+      
+        return Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
+
+    let itemId = 0;
 
     return (
         <div>
-            <Header />
-            <div className="container-form">
+            <div className="">
                 <h1>Table of participants</h1>
 
                 <table>
                     <thead>
-                        <th className="name-column">Name</th>
-                        <th className="age-column">Age</th>
-                        <th className="email-column">Email</th>
-                        <th className="hometown-column">Hometown</th>
-                        <th className="hobbies-column">Hobbies/Interest</th>
-                        <th className="buddy-column">Buddy or patient?</th>
+                        <tr>
+                            <th className="name-column">Name</th>
+                            <th className="age-column">Age</th>
+                            <th className="email-column">Email</th>
+                            <th className="hometown-column">Hometown</th>
+                            <th className="hobbies-column">Hobbies/Interest</th>
+                            <th className="buddy-column">Buddy or patient?</th>
+                        </tr>
                     </thead>
                     <tbody>
-                        {listOfPeople.map( person => (
-                            <tr key={person.id} className={person.buddy_patient}>
+                        {props.data.map( person => {
+                            itemId++;
+                            
+                            let buddy_patient = "patient";
+
+                            const age = calculate_age(person.dateofbirth);
+
+                            if(person.im_a_buddy === 1)  buddy_patient = "buddy";
+
+                            return (<tr key={itemId} className={buddy_patient}>
                                 <td className="name-column">{person.name}</td>
-                                <td className="age-column">{person.age}</td>
+                                <td className="age-column">{age}</td>
                                 <td className="email-column">{person.email}</td>
                                 <td className="hometown-column">{person.hometown}</td>
-                                <td className="hobbies-column">{person.hobbies}</td>
-                                <td className="buddy-column">{person.buddy_patient}</td>
+                                <td className="hobbies-column">{person.hobbiesandinterests}</td>
+                                <td className="buddy-column">{buddy_patient}</td>
                             </tr>
-                        ) )}
+                        )} )}
                     </tbody>
                 </table>
             </div>
@@ -45,4 +103,4 @@ const AdminTable = () => {
     );
 }
 
-export default AdminTable;
+export default AdminTableFetcher;
